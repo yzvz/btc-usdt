09eb35734901c83c206b31572593798a96ea96b6
Yaroslav <yaroslav.shell@gmail.com>
2019-05-22 13:36:43 -0400

    Optimal buffered entries count

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,6 @@ export const TICKER_WS_URL = 'wss://stream.binance.com:9443/stream?streams=btcus
 export const ORDERBOOK_WS_URL = 'wss://stream.binance.com:9443/stream?streams=btcusdt@depth';
 export const ORDERBOOK_SNAPSHOT_URL = 'https://www.binance.com/api/v1/depth?symbol=BTCUSDT&limit=500';
 
-export const MAX_BUFFERED_TRADES = 100;
-export const MAX_BUFFERED_ASKS = 100;
-export const MAX_BUFFERED_BIDS = 100;
+export const MAX_BUFFERED_TRADES = 200;
+export const MAX_BUFFERED_ASKS = 50;
+export const MAX_BUFFERED_BIDS = 50;
